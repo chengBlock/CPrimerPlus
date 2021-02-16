db12bd59cda646b2ab27440bf880e5e90dc221d4
clcheng <l499886834@163.com>
2021-02-16 10:47:07 +0800

    argc获取命令行的参数个数，argv是可变参数列表，可以获取命令行参数，./count本身也算一个参数

--- chapter13/count.c
+++ chapter13/count.c
@@ -1,9 +1,10 @@
 // count.c -- 使用标准 I/O
 #include <stdio.h>
-#include <stdlib.h>     //提供exit()的原型
+#include <stdlib.h> //提供exit()的原型
 
 int main(int argc, char const *argv[])
 {
+    printf("argc is %d\n", argc);
     // 读取文件时，储存每个字符的地方
     int ch;
     // 文件指针
@@ -11,23 +12,29 @@ int main(int argc, char const *argv[])
     unsigned long count = 0;
     if (argc != 2)
     {
-        printf("Usage: %s filename\n",argv[0]);
+        printf("Usage: %s filename\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    if ((fp = fopen(argv[1],"r")) == NULL)
+    if ((fp = fopen(argv[1], "r")) == NULL)
     {
-        printf("Can't open %s\n",argv[1]);
+        printf("Can't open %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
     while ((ch = getc(fp) != EOF))
     {
         // 将读入的字符标准输出，等同于putchar
-        putc(ch,stdout);
+        // ?:为啥不输出
+        putc(ch, stdout);
         count++;
     }
-    fclose(fp);
 
-    printf("File %s has %lu characters.\n",argv[1],count);
-    
+    // 文件关闭成功会返回0，否则返回EOF
+    // 如果磁盘已满或被移除、出现IO错误，都会导致fclose调用失败
+    if (fclose(fp) != 0)
+    {
+        printf("Error in closing file %s\n", argv[1]);
+    }
+
+    printf("File %s has %lu characters.\n", argv[1], count);
     return 0;
 }
